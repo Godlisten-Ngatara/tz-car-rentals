cedb01679da4a62883de0f876df090993891ca18
Godlisten-Ngatara <gpn0021@gmail.com>
2025-05-27 09:43:26 +0300

    Feat(bookings): Add a booking model

---
+++ models/bookings.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    car: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Car'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    pickUpDate: {
+        type: Date,
+        required: true
+    },
+    dropOffDate: {
+        type: Date,
+        required: true
+    },
+    pickUpLocation: {
+        type: String,
+        required: true
+    },
+    dropOffLocation: {
+        type: String,
+        required: true
+    },
+    totalPrice: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["notBooked", "booked", "onTrip", "notPicked", "canceled", "overDue", "tripCompleted"],
+        default: "notBooked"
+    }
+},
+    { timestamps: true }
+)
+
+const Bookings = mongoose.model('Bookings', bookingSchema)
+export default Bookings
